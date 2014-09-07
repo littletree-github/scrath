80585338fde04878cdd7be1d01730890024f7e90
saturn <you@example.com>
2014-09-07 16:53:12 -0700

    1. dmatrix -> matrix; 2. predict functions

--- nn/soft_max.py
+++ nn/soft_max.py
@@ -1,25 +1,41 @@
+#!/usr/bin/env python
+
 import numpy
 import theano
 import theano.tensor as T
+import os
 
- 
-def theano_softmax():
-    M = T.dmatrix("M")
+
+def generate_theano_softmax():
+    M = T.matrix("M")
     y = T.nnet.softmax(M)
     softmax_callable = theano.function([M], y)
     return softmax_callable
  
+theano_softmax = generate_theano_softmax()
  
-def theano_p_y_given_x():
-    X = T.dmatrix('X')
-    W = T.dmatrix('W')
-    b = T.dmatrix('b')
-    input = T.dot(X, W) + b
-    y = T.nnet.softmax(input)
+def generate_theano_p_y_given_x():
+    X = T.matrix('X')
+    W = T.matrix('W')
+    b = T.matrix('b')
+    y = T.nnet.softmax(T.dot(X, W) + b)
     p_y_callable = theano.function([X, W, b], y)
     return p_y_callable
- 
- 
+
+theano_p_y_given_x = generate_theano_p_y_given_x()
+
+
+def generate_theano_predict():
+    X = T.matrix('X')
+    W = T.matrix('W')
+    b = T.matrix('b')
+    y = T.nnet.softmax(T.dot(X, W) + b)
+    y_pred = T.argmax(y, axis=1)
+    return theano.function([X, W, b], y_pred)
+
+theano_predict = generate_theano_predict()
+
+
 def softmax(M):
     M = numpy.array(M)
     row_max = numpy.amax(M, axis=1)
@@ -27,8 +43,14 @@ def softmax(M):
     cwise_exp = numpy.exp(M - row_max)
     v_softmax = cwise_exp / numpy.sum(cwise_exp, axis=1).reshape(row_max.shape[0], 1)
     return v_softmax
- 
-def p_y_given_x(X, w, b):
-    dt = numpy.dot(X, w) + b
+
+
+def p_y_given_x(X, W, b):
+    dt = numpy.dot(X, W) + b
     return softmax(dt)
  
+def predict(X, W, b):
+    y = p_y_given_x(X, W, b)
+    return numpy.argmax(y, axis=1)
+
+
--- nn/tst/test_softmax.py
+++ nn/tst/test_softmax.py
@@ -7,14 +7,23 @@ from nose.tools import ok_
 def test_soft_max():
     seed(12345)
     X = randn(10, 10) 
-    w = randn(10, 10)
+    W = randn(10, 10)
     b = randn(10, 10)
-    smx_th = soft_max.theano_softmax()(X)
+    smx_th = soft_max.theano_softmax(X)
     smx = soft_max.softmax(X)
     
     ok_(allclose(smx_th, smx), "theano and numpy softmax should generate same result for same input")
-    th_py = soft_max.theano_p_y_given_x()(X, w, b)
-    py = soft_max.p_y_given_x(X, w, b)
+    th_py = soft_max.theano_p_y_given_x(X, W, b)
+    py = soft_max.p_y_given_x(X, W, b)
     ok_(allclose(th_py, py), "theano and numpy softmax should generate same proba for same input")
 
+def test_prediction():
+    seed(12345)
+    X = randn(10, 10) 
+    W = randn(10, 10)
+    b = randn(10, 10)
 
+    theano_predict =  soft_max.theano_predict(X, W, b)
+    numpy_predict = soft_max.predict(X, W, b)
+    ok_(allclose(theano_predict, numpy_predict))
+    # print soft_max.theano_prediction_error(predict, predict)
